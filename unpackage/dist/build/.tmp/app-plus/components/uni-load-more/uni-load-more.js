0dc57d96bcf411c57e5ed201bc9a9036efd4a91e
zhuzy <373712896@qq.com>
2020-08-25 11:19:06 +0800

    2

---
+++ unpackage/dist/build/.tmp/app-plus/components/uni-load-more/uni-load-more.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/uni-load-more/uni-load-more"],{"29e7":function(t,n,e){},"2c69":function(t,n,e){"use strict";(function(t){Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var e=t.getSystemInfoSync().platform,o={name:"UniLoadMore",props:{status:{type:String,default:"more"},showIcon:{type:Boolean,default:!0},iconType:{type:String,default:"auto"},color:{type:String,default:"#777777"},contentText:{type:Object,default:function(){return{contentdown:"上拉显示更多",contentrefresh:"正在加载...",contentnomore:"没有更多数据了"}}}},data:function(){return{platform:e}}};n.default=o}).call(this,e("6e42")["default"])},"63d2":function(t,n,e){"use strict";e.r(n);var o=e("2c69"),u=e.n(o);for(var r in o)"default"!==r&&function(t){e.d(n,t,(function(){return o[t]}))}(r);n["default"]=u.a},b99b:function(t,n,e){"use strict";e.r(n);var o=e("df89"),u=e("63d2");for(var r in u)"default"!==r&&function(t){e.d(n,t,(function(){return u[t]}))}(r);e("bd69");var a,c=e("f0c5"),f=Object(c["a"])(u["default"],o["b"],o["c"],!1,null,null,null,!1,o["a"],a);n["default"]=f.exports},bd69:function(t,n,e){"use strict";var o=e("29e7"),u=e.n(o);u.a},df89:function(t,n,e){"use strict";var o,u=function(){var t=this,n=t.$createElement;t._self._c},r=[];e.d(n,"b",(function(){return u})),e.d(n,"c",(function(){return r})),e.d(n,"a",(function(){return o}))}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/uni-load-more/uni-load-more-create-component',
+    {
+        'components/uni-load-more/uni-load-more-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('6e42')['createComponent'](__webpack_require__("b99b"))
+        })
+    },
+    [['components/uni-load-more/uni-load-more-create-component']]
+]);
---
+++ unpackage/dist/build/app-plus/pages/order/order.js
@@ -0,0 +1,2 @@
+setCssToHead(["body{background: #ededed;}\n",],undefined,{path:"./pages/order/order.wxss"})();
+document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/order/order.wxml') } }));
